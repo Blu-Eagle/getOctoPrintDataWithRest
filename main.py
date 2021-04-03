85dc164d144e166e2b0ec48447246ecbd705728e
Blu-Eagle <32535727+Blu-Eagle@users.noreply.github.com>
2021-04-03 17:36:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 
 
-url='http://192.168.1.30/api/job?apikey=D643A7C8C0E24575B84C7A55D0313C79'
+url='http://192.168.1.30/api/job?apikey=0000000000000000000000000'
 data = requests.get(url, headers={'accept': 'application/json'})
 #print(data.json())
 data = data.json()
 print(f"status: {data['state']} -> progress: {int(data['progress']['completion']*100)/100}%")
 
 
-url='http://192.168.1.30/api/printer?apikey=D643A7C8C0E24575B84C7A55D0313C79'
+url='http://192.168.1.30/api/printer?apikey=000000000000000000000000000'
 data = requests.get(url, headers={'accept': 'application/json'})
 #print(data.json())
 data = data.json()
